ade3a74d2fff28cbccd68ec043a0da2f7c4c52ab
Matthew Hunger <matt.hunger@gmail.com>
2017-09-23 11:15:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -115,16 +115,16 @@ function endGame() {
 
 function moveDodger(e) {
 
-  document.addEventListener('keydown', function(e) {
+  //document.addEventListener('keydown', function(e) {
     if (e.which === LEFT_ARROW) {
       e.preventDefault()
       e.stopPropagation()
       moveDodgerLeft()
-      
+
     } else if (e.which === RIGHT_ARROW) {
       moveDodgerRight()
     }
-  })
+  //})
 
 }
 
